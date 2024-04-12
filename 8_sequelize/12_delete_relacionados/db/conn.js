479b5364f1c91d88a5256c59176e3499d0418586
arthurfarias <arthurfarias@arthurfarias.fariasarthur>
2024-04-12 10:57:36 -0300

    att

---
+++ 8_sequelize/12_delete_relacionados/db/conn.js
@@ -0,0 +1,15 @@
+const { Sequelize } = require("sequelize");
+
+const sequelize = new Sequelize("nodesequelize2", "postgres", "50261014", {
+  host: "localhost",
+  dialect: "postgres",
+});
+
+try {
+  sequelize.authenticate();
+  console.log("Conectamos com sucesso utilizando sequelize");
+} catch (err) {
+  console.log("Não foi possível conectar no banco de dados", err);
+}
+
+module.exports = sequelize;
